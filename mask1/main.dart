15cdb7345b30794679a946c975c2936955ac307f
Piyush-Pegasus <108669970+Piyush-Pegasus@users.noreply.github.com>
2023-04-16 17:51:59 +0530

    Add files via upload

---
+++ mask1/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import './homescreen.dart';
+
+void main(List<String> args) {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
